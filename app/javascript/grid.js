91a7d854fcd79707e8864e4ec8b5c3040c778ad2
Phil White <pjd.white@gmail.com>
2018-03-02 20:47:38 +0000

    implement tests

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -3,20 +3,21 @@ const MAX_X = 10;
 const MAX_Y = 10;
 
 export class Block {
-  constructor(x, y) {
+  constructor(x, y, colour) {
     this.x = x;
     this.y = y;
-    this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+    this.colour = colour || COLOURS[Math.floor(Math.random() * COLOURS.length)];
   }
 }
 
 export class BlockGrid {
-  constructor() {
+  constructor(width, height) {
     this.grid = [];
+    this.rendered = [];
 
-    for (let x = 0; x < MAX_X; x++) {
+    for (let x = 0; x < width; x++) {
       let col = [];
-      for (let y = 0; y < MAX_Y; y++) {
+      for (let y = 0; y < height; y++) {
         col.push(new Block(x, y));
       }
 
@@ -27,14 +28,15 @@ export class BlockGrid {
   }
 
   render(el = document.querySelector('#gridEl')) {
-    for (let x = 0; x < MAX_X; x++) {
+
+    for (let x = 0; x < this.grid.length; x++) {
       let id = 'col_' + x;
       let colEl = document.createElement('div');
       colEl.className = 'col';
       colEl.id = id;
       el.appendChild(colEl);
 
-      for (let y = MAX_Y - 1; y >= 0; y--) {
+      for (let y = this.grid[x].length - 1; y >= 0; y--) {
         let block = this.grid[x][y],
           id = `block_${x}x${y}`,
           blockEl = document.createElement('div');
@@ -45,6 +47,8 @@ export class BlockGrid {
         blockEl.addEventListener('click', evt => this.blockClicked(evt, block));
         colEl.appendChild(blockEl);
       }
+
+      this.rendered.push(colEl);
     }
 
     return this;
@@ -55,4 +59,4 @@ export class BlockGrid {
   }
 }
 
-window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
+window.addEventListener('DOMContentLoaded', () => new BlockGrid(MAX_X, MAX_Y).render());
--- app/javascript/grid.test.js
+++ app/javascript/grid.test.js
@@ -1,5 +1,11 @@
-import { Block, COLOURS } from './grid';
+import { Block, BlockGrid, COLOURS } from './grid';
 import { assert } from 'chai';
+import sinon from 'sinon';
+import jsdom from 'jsdom';
+import fs from 'fs';
+import path from 'path';
+
+const html = fs.readFileSync(path.resolve('./public/index.html'),'utf8');
 
 describe('Block', () => {
   it('should be created with correct coordinates and one of the valid colours', () => {
@@ -15,5 +21,72 @@ describe('Block', () => {
 });
 
 describe('BlockGrid', () => {
-  xit('should be implemented!', () => {});
+  
+  let sandbox;
+  before(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  })
+
+  it('should construct a game grid of the correct size', () => {
+    const blockGrid = new BlockGrid(10, 10);
+    assert.lengthOf(blockGrid.grid, 10, 'grid has width of 10');
+    assert.lengthOf(blockGrid.grid[0], 10, 'grid has height of 10');
+  });
+
+  it('should render a valid game from the grid', async () => {
+    const testColumn = Array(4).fill(new Block(0, 0, 'red'));
+    const testGrid = {
+      grid: Array(4).fill(testColumn),
+      rendered: []
+    };
+    
+    await new Promise((resolve, reject) => {
+      jsdom.env(html, [], (err, window) => {
+        BlockGrid.prototype.render.apply(testGrid, [window.document.querySelector('#gridEl')]);
+        resolve();
+      })
+    });
+    
+    assert.lengthOf(testGrid.rendered, 4, 'rendered game has height of 10');
+
+    const firstCol = testGrid.rendered[0];
+    assert.lengthOf(firstCol.children, 4, 'rendered game has width of 10');
+    assert.equal(firstCol.children[0].style.background, 'red', 'rendered game tile has correct colour');
+  });
+
+  it('should remove co-located elements of the same colour from the grid when an element is clicked', () => {
+    const testColumnA = Array(4).fill();
+    const testGrid = Object.assign({}, BlockGrid, {
+      grid: [
+        [new Block(0, 0, 'red'), new Block(0, 0, 'green'), new Block(0, 0, 'red')],
+        [new Block(0, 0, 'green'), new Block(0, 0, 'green'), new Block(0, 0, 'green')],
+        [new Block(0, 0, 'red'), new Block(0, 0, 'green'), new Block(0, 0, 'red')],
+      ],
+      rendered: [],
+      render: () => {}
+    });
+
+    BlockGrid.prototype.blockClicked.apply(testGrid, [{}, new Block(1, 1, 'green')]);
+
+    assert.lengthOf(testGrid.grid, 3, 'rendered game still has 3 columns');
+    assert.lengthOf(testGrid.grid[0], 2, 'column 1 has 2 blocks');
+    assert.lengthOf(testGrid.grid[1], 0, 'column 2 has 0 blocks');
+    assert.lengthOf(testGrid.grid[2], 2, 'column 3 has 2 blocks');
+
+    assert.deepEqual(testGrid.grid[0][0], new Block(1, 1, 'red'), 'column 1 block 1 is red');
+    assert.deepEqual(testGrid.grid[0][1], new Block(1, 1, 'red'), 'column 1 block 2 is red');
+    assert.deepEqual(testGrid.grid[2][0], new Block(1, 1, 'red'), 'column 3 block 1 is red');
+    assert.deepEqual(testGrid.grid[2][1], new Block(1, 1, 'red'), 'column 3 block 2 is red');
+  });
+
+  it('should rerender the game grid when the grid has changed', () => {
+    const blockGrid = new BlockGrid(10, 10);
+    const renderSpy = sandbox.stub(blockGrid, 'render');
+    blockGrid.blockClicked({}, blockGrid.grid[0][0]);
+    sinon.assert.calledOnce(renderSpy);
+  });
 });
