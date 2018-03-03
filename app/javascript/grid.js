fe2d089686343eb4e27b858c9ccb37ba346e13e6
Phil White <pjd.white@gmail.com>
2018-03-03 15:31:20 +0000

    first iteration tests pass!

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -10,15 +10,16 @@ export class Block {
 
 export class BlockGrid {
   constructor(width, height) {
-    this.rendered = [];
     this.grid = Array(width).fill([]).map(() => {
       return Array(height).fill([]).map(() => new Block());
     });
+    this.rendered = [];
+    this.height = height;
     return this;
   }
 
   render(container = document.querySelector('#gridEl')) {
-    container.innerHTML = "";
+    container.innerHTML = '';
 
     this.grid.forEach((column, columnId) => {
       const columnElement = document.createElement('div');
@@ -27,14 +28,16 @@ export class BlockGrid {
       container.appendChild(columnElement);
 
       column.forEach((block, rowId) => {
+        const blockHeight = columnElement.offsetHeight / this.height;
         const blockElement = document.createElement('div');
         blockElement.className = 'block';
         blockElement.style.background = block.colour;
+        blockElement.style.bottom = blockHeight * rowId;
         blockElement.addEventListener('click', () => {
           this.blockClicked(block, {
             xCoord: columnId,
             yCoord: rowId
-          })
+          });
         });
 
         columnElement.appendChild(blockElement);
@@ -46,8 +49,38 @@ export class BlockGrid {
   }
 
   blockClicked(block, { xCoord, yCoord }) {
-    console.log('%o x:%i y:%i', block, xCoord, yCoord);
+    function selectAdjacentByColour(grid, colourToMatch, originX, originY) {
+      [
+        [originX, originY],
+        [originX, originY + 1],
+        [originX, originY - 1],
+        [originX - 1, originY],
+        [originX + 1, originY]
+      ].forEach(([x, y]) => {
+        if (
+          grid[x] &&
+          grid[x][y] &&
+          grid[x][y].colour == colourToMatch &&
+          !grid[x][y].selected
+        ) {
+          grid[x][y].selected = true;
+          grid = selectAdjacentByColour(grid, colourToMatch, x, y);
+        }
+      });
+      return grid;
+    }
+
+    this.grid = selectAdjacentByColour(
+      this.grid,
+      block.colour,
+      xCoord,
+      yCoord
+    ).map(column => column.filter(block => !block.selected));
+
+    this.render();
+    return this;
   }
 }
 
-window.addEventListener('DOMContentLoaded', () => new BlockGrid(MAX_X, MAX_Y).render());
+window.addEventListener('DOMContentLoaded', () =>
+  new BlockGrid(MAX_X, MAX_Y).render());
--- app/javascript/grid.test.js
+++ app/javascript/grid.test.js
@@ -5,7 +5,7 @@ import jsdom from 'jsdom';
 import fs from 'fs';
 import path from 'path';
 
-const html = fs.readFileSync(path.resolve('./public/index.html'),'utf8');
+const html = fs.readFileSync(path.resolve('./public/index.html'), 'utf8');
 
 describe('Block', () => {
   it('should randomly select a colour if none is provided', () => {
@@ -15,7 +15,6 @@ describe('Block', () => {
 });
 
 describe('BlockGrid', () => {
-  
   let sandbox;
   before(() => {
     sandbox = sinon.createSandbox();
@@ -23,7 +22,7 @@ describe('BlockGrid', () => {
 
   afterEach(() => {
     sandbox.restore();
-  })
+  });
 
   it('should construct a game grid of the correct size', () => {
     const blockGrid = new BlockGrid(10, 10);
@@ -37,35 +36,41 @@ describe('BlockGrid', () => {
       grid: Array(4).fill(testColumn),
       rendered: []
     };
-    
+
     await new Promise((resolve, reject) => {
       jsdom.env(html, [], (err, window) => {
-        BlockGrid.prototype.render.apply(testGrid, [window.document.querySelector('#gridEl')]);
+        BlockGrid.prototype.render.apply(testGrid, [
+          window.document.querySelector('#gridEl')
+        ]);
         resolve();
-      })
+      });
     });
-    
+
     assert.lengthOf(testGrid.rendered, 4, 'rendered game has height of 10');
 
     const firstCol = testGrid.rendered[0];
     assert.lengthOf(firstCol.children, 4, 'rendered game has width of 10');
-    assert.equal(firstCol.children[0].style.background, 'red', 'rendered game tile has correct colour');
+    assert.equal(
+      firstCol.children[0].style.background,
+      'red',
+      'rendered game tile has correct colour'
+    );
   });
 
-  it('should remove co-located elements of the same colour from the grid when an element is clicked', () => {
+  it('should remove adjacent elements of the same colour from the grid when an element is clicked', () => {
     const testColumnA = Array(4).fill();
     const testGrid = Object.assign({}, BlockGrid, {
       grid: [
         [new Block(COLOURS[0]), new Block(COLOURS[1]), new Block(COLOURS[0])],
         [new Block(COLOURS[1]), new Block(COLOURS[1]), new Block(COLOURS[1])],
-        [new Block(COLOURS[0]), new Block(COLOURS[1]), new Block(COLOURS[0])],
+        [new Block(COLOURS[0]), new Block(COLOURS[1]), new Block(COLOURS[0])]
       ],
       rendered: [],
       render: () => {}
     });
 
     BlockGrid.prototype.blockClicked.apply(testGrid, [
-      new Block(COLOURS[1]), 
+      new Block(COLOURS[1]),
       { xCoord: 0, yCoord: 0 }
     ]);
 
@@ -74,10 +79,26 @@ describe('BlockGrid', () => {
     assert.lengthOf(testGrid.grid[1], 0, 'column 2 has 0 blocks');
     assert.lengthOf(testGrid.grid[2], 2, 'column 3 has 2 blocks');
 
-    assert.deepEqual(testGrid.grid[0][0], new Block(COLOURS[0]), 'column 1 block 1 is red');
-    assert.deepEqual(testGrid.grid[0][1], new Block(COLOURS[0]), 'column 1 block 2 is red');
-    assert.deepEqual(testGrid.grid[2][0], new Block(COLOURS[0]), 'column 3 block 1 is red');
-    assert.deepEqual(testGrid.grid[2][1], new Block(COLOURS[0]), 'column 3 block 2 is red');
+    assert.deepEqual(
+      testGrid.grid[0][0],
+      new Block(COLOURS[0]),
+      'column 1 block 1 is red'
+    );
+    assert.deepEqual(
+      testGrid.grid[0][1],
+      new Block(COLOURS[0]),
+      'column 1 block 2 is red'
+    );
+    assert.deepEqual(
+      testGrid.grid[2][0],
+      new Block(COLOURS[0]),
+      'column 3 block 1 is red'
+    );
+    assert.deepEqual(
+      testGrid.grid[2][1],
+      new Block(COLOURS[0]),
+      'column 3 block 2 is red'
+    );
   });
 
   it('should rerender the game grid when the grid has changed', () => {
